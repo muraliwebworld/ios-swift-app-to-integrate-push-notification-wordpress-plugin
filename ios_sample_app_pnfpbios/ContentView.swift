8474485df14392ff2959b6f968cd470fc14107fe
Muralidharan Ramasamy <murali@indiacitys.com>
2023-04-26 19:15:33 +0530

    Initial Commit

---
+++ ios_sample_app_pnfpbios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ios_sample_app_pnfpbios
+//
+//  Created by Muralidharan Ramasamy on 26/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_sample_app_pnfpbios/ios_sample_app_pnfpbiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_sample_app_pnfpbiosApp.swift
+//  ios_sample_app_pnfpbios
+//
+//  Created by Muralidharan Ramasamy on 26/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ios_sample_app_pnfpbiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
